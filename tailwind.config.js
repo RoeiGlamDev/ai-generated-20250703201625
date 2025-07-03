a4c75c87becd9da74054d97bd20e9e1f460fd3cf
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 03:16:27 +0700

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#ffd700',
+      },
+    },
+  },
+  plugins: [],
+};
